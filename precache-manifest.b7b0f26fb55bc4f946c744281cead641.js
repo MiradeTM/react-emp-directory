c96cbcb519d46bea972cdb17c47ec7dba285e499
Mirade McGloin <mirade.mcgloin@gmail.com>
2020-08-31 23:43:21 -0600

    Updates

---
+++ precache-manifest.b7b0f26fb55bc4f946c744281cead641.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72fbf34934dbbf78ab02372f8055e42f",
+    "url": "/react-emp-directory/index.html"
+  },
+  {
+    "revision": "46b2a098ec87a971efe1",
+    "url": "/react-emp-directory/static/css/main.a9cccad3.chunk.css"
+  },
+  {
+    "revision": "c66283bf50cb1a799d12",
+    "url": "/react-emp-directory/static/js/2.c48ff98f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-emp-directory/static/js/2.c48ff98f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46b2a098ec87a971efe1",
+    "url": "/react-emp-directory/static/js/main.1b2c9650.chunk.js"
+  },
+  {
+    "revision": "9cd704d0983f10f4a97e",
+    "url": "/react-emp-directory/static/js/runtime-main.4591019e.js"
+  }
+]);
